833001f8b491606a1327a92973298c09052ded4b
caboteja <34185531+caboteja@users.noreply.github.com>
2018-11-02 00:02:11 -0600

    edited titles on ids and names

--- app/src/main/java/com/team7/cmput301/android/theirisproject/AddProblemActivity.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/AddProblemActivity.java
@@ -3,11 +3,11 @@ package com.team7.cmput301.android.theirisproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Create_ProbActivity extends AppCompatActivity {
+public class AddProblemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_prob);
+        setContentView(R.layout.acitvity_addproblem);
     }
 }
