098975f8833b018a1d7d27c15db2140c51f0867f
anticobalt <anticobalt@users.noreply.github.com>
2018-11-03 20:40:16 -0600

    Remove Model usage from initialization of IrisController

--- app/src/main/java/com/team7/cmput301/android/theirisproject/controller/IrisController.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/controller/IrisController.java
@@ -13,33 +13,17 @@ import com.team7.cmput301.android.theirisproject.IrisActivity;
 
 /**
  * A type of controller that is used by an IrisActivity to manage a Model object. The IrisController
- * is responsible for updating the Model object, interpreting the Model object, and binding it to
- * an Intent so that the IrisActivity can start another IrisActivity.
+ * is responsible for updating and interpreting the Model object, as well as saving it to databases.
  *
  * @author anticobalt
  * @see IrisActivity
  */
 public abstract class IrisController {
 
-    protected Parcelable model;
+    protected Object model;
 
-    /**
-     * The constructor of an IrisController subclass can take in a specific Model class
-     * (e.g. Problem) and use it make a call to super(), as long as that Model implements Parcelable.
-     * @param model Any Parcelable object
-     */
-    IrisController(Parcelable model){
-        this.model = model;
-    }
-
-    /**
-     * Put the associated model object into the passed Intent, and return it.
-     * @param intent Calling Activity's Intent
-     * @return Modified Intent
-     */
-    public Intent bindModelToIntent(Intent intent){
-        intent.putExtra("model", model);
-        return intent;
-    }
+    abstract void makeModelFromDB();
+    abstract void saveModelToLocalDB();
+    abstract void saveModelToOnlineDB();
 
 }
