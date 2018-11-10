8495077e7a61ff8f4a130ac02d0dcc91cef296c2
anticobalt <anticobalt@users.noreply.github.com>
2018-11-10 14:54:44 -0700

    Add RecordListActivity/Controller

---
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/controller/RecordListController.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Team 7, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ *
+ *
+ */
+
+package com.team7.cmput301.android.theirisproject.controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class RecordListController extends IrisController {
+
+    public RecordListController(Intent intent){
+        super(intent);
+    }
+
+    @Override
+    Object getModel(Bundle data) {
+        return null;
+    }
+}
