18965f3b8c41925c4463e6ce337c0e178f185d8f
itstc <itsthomaschu@gmail.com>
2018-11-11 16:57:23 -0700

    fixed comment typo PR #50

--- app/src/main/java/com/team7/cmput301/android/theirisproject/AddProblemActivity.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/AddProblemActivity.java
@@ -38,6 +38,7 @@ public class AddProblemActivity extends IrisActivity {
 
             @Override
             public void onClick(View view) {
+                // send new problem to database, callback has result true if successful, else false
                 controller.submitProblem(name.getText().toString(), desc.getText().toString(), new Callback<Boolean>() {
                     @Override
                     public void onComplete(Boolean success) {
