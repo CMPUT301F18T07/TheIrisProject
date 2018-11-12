3168b80c8752c93d5ee79bbbe256cc4e1da84c9e
itstc <itsthomaschu@gmail.com>
2018-11-11 17:12:42 -0700

    userID fix

--- app/src/main/java/com/team7/cmput301/android/theirisproject/model/User.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/model/User.java
@@ -25,10 +25,6 @@ public abstract class User {
         this.type = type;
     }
 
-    public String getId() {
-        return _id;
-    }
-
     public void setId(String _id) {
         this._id = _id;
     }
