52814d82a13c3cbf4ad023b3c58be452a9366e14
jtfwong <43226692+jtfwong@users.noreply.github.com>
2018-11-13 01:14:04 -0700

    Rough layouts for records (#71)
    * Skeleton code
    
    * Skeleton code
    
    * Skeleton code revised
    
    * Skeleton code revised 2
    
    * Record tests
    
    * add record activity
    
    * catch up
    
    * Added rough layouts

---
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/AddRecordActivity.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Team 7, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ */
+
+package com.team7.cmput301.android.theirisproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AddRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_record);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
