125e3bcfd7c727694e8cbe896d7500f0bdb98c2f
caboteja <34185531+caboteja@users.noreply.github.com>
2018-11-14 11:08:28 -0700

    added comment blocks to activities

--- app/src/main/java/com/team7/cmput301/android/theirisproject/activity/AddProblemActivity.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/activity/AddProblemActivity.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Team 7, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ *
+ *
+ */
+
 package com.team7.cmput301.android.theirisproject.activity;
 
 import android.content.Intent;
