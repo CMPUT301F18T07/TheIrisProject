3b8036a932f1e39a82d56c3668ef9e50fc9b31fa
Thomas Chu <itsthomaschu@gmail.com>
2018-11-14 21:23:02 -0700

    Revert "edit profile and view profile screens for patient and care providers (#73)" (#79)
    This reverts commit 08f42890a5a598972f861c63e466a97d6fa17ee1.

--- app/src/main/java/com/team7/cmput301/android/theirisproject/activity/AddProblemActivity.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/activity/AddProblemActivity.java
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) Team 7, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
- *
- *
- */
-
 package com.team7.cmput301.android.theirisproject.activity;
 
 import android.content.Intent;
