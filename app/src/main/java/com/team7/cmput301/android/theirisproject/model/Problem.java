a20f88d0414fdd38eeab3668339d6bf0b4fe93b8
anticobalt <anticobalt@users.noreply.github.com>
2018-11-18 15:43:55 -0700

    Update tests to work with updated models (#108)
    * Prevent duplicate login
    
    * Add copyright
    
    * Add Patient binding in GetUserDataTask, CareProvider.addPatient(), minor fixes
    
    * Integrate GetUserDataTask into LoginActivity
    
    * Fix LoginTask typo; add CareProvider setter; refactor; start bindProblems
    
    * Finish GetUserDataTask
    
    Includes adjustments to models to work with GetUserDataTask, and removal of BodyLocation model.
    Most notably, all models are initialized with their parent's ID, so that they can be searched for.
    
    * Add original BodyPhoto constructor back because I broke everything
    
    * Revert Problem's userId -> user, minor string fixes
    
    * Fix tests and models
    
    * Refactor to camel-case, cleanup

--- app/src/main/java/com/team7/cmput301/android/theirisproject/model/Problem.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/model/Problem.java
@@ -36,7 +36,7 @@ public class Problem {
     private String title;
     private Date date;
     private String description;
-    transient private RecordList records;
+    transient private RecordList records = new RecordList();
     transient private List<Comment> comments = new ArrayList<>();
     transient private List<BodyPhoto> bodyPhotos = new ArrayList<>();
 
@@ -67,7 +67,7 @@ public class Problem {
 
     }
 
-    /* Basic setters */
+    /* Basic setter + adders */
 
     public void setRecords(RecordList records){
         this.records = records;
@@ -77,6 +77,14 @@ public class Problem {
 
     public void setComments(List<Comment> comments) {
         this.comments = comments;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public void addBodyPhoto(BodyPhoto bodyPhoto) {
+        bodyPhotos.add(bodyPhoto);
     }
 
     /* Basic getters */
