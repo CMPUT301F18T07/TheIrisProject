7101a88d8f0000f87b95cb1f0ba8eeab43d4e634
anticobalt <anticobalt@users.noreply.github.com>
2018-11-19 17:31:09 -0700

    Add DeleteProblemActivityTest (#116)
    * Add DeleteProblemActivityTest
    
    * Make flag no static

---
+++ app/src/androidTest/java/com/team7/cmput301/android/theirisproject/DeleteProblemActivityTest.java
@@ -0,0 +1,94 @@
+package com.team7.cmput301.android.theirisproject;
+
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.Button;
+
+import com.robotium.solo.Solo;
+import com.team7.cmput301.android.theirisproject.activity.DeleteProblemActivity;
+import com.team7.cmput301.android.theirisproject.activity.ProblemListActivity;
+import com.team7.cmput301.android.theirisproject.helper.Timer;
+import com.team7.cmput301.android.theirisproject.model.Problem;
+import com.team7.cmput301.android.theirisproject.task.AddProblemTask;
+import com.team7.cmput301.android.theirisproject.task.Callback;
+
+/**
+ * Tests for EditPatientProfileActivity
+ *
+ * @author anticobalt
+ */
+public class DeleteProblemActivityTest extends ActivityInstrumentationTestCase2<DeleteProblemActivity> {
+
+    int flag = 0;
+
+    private Solo solo;
+    private String title = "I hit my head";
+    private String desc = "I have amnesia now";
+    private String patientId = "pid";
+
+    public DeleteProblemActivityTest() {
+        super(DeleteProblemActivity.class);
+    }
+
+    @Override
+    protected void setUp() {
+
+        // make problem
+        Problem problem = new Problem(title, desc, patientId);
+
+        // make a callback that signals task completion
+        Callback callback = new Callback(){
+            @Override
+            public void onComplete(Object res) {
+                setFlag(1);
+            }
+        };
+
+        // put patientID in Intent, as required for IrisActivity
+        Intent intent = new Intent();
+        intent.putExtra("patientId", patientId);
+        setActivityIntent(intent);
+
+        // add problem
+        new AddProblemTask(callback).execute(problem);
+
+        // start Solo
+        solo = new Solo(getInstrumentation(), getActivity());
+
+    }
+
+    @Override
+    protected void tearDown() {
+        solo.finishOpenedActivities();
+    }
+
+    public void testCorrectActivity() {
+        String activityName = DeleteProblemActivity.class.getSimpleName();
+        solo.assertCurrentActivity("Wrong activity!", activityName);
+    }
+
+    public void testDeleteProblem() {
+
+        // Wait for task to finish
+        while (flag == 0){
+            Timer.sleep(100);
+        }
+        flag = 0;
+
+        // click delete button
+        View deleteButton = solo.getView(R.id.do_delete_problem_button);
+        solo.clickOnView(deleteButton);
+
+        // confirm the activity closes and Problem isn't on list view
+        solo.waitForActivity(ProblemListActivity.class);
+        assertFalse(solo.searchText(title));
+        assertFalse(solo.searchText(desc));
+
+    }
+
+    private void setFlag(int value) {
+        flag = value;
+    }
+
+}
