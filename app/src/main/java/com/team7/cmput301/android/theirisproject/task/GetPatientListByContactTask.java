5df46e2d1287c99a8f7f0706e3386a2d2ea4016f
Justin Mah <jmmxpdev@gmail.com>
2018-11-25 13:17:57 -0700

    Improved query speed by using terms

--- app/src/main/java/com/team7/cmput301/android/theirisproject/task/GetPatientListByContactTask.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/task/GetPatientListByContactTask.java
@@ -7,6 +7,7 @@ package com.team7.cmput301.android.theirisproject.task;
 import android.os.AsyncTask;
 
 import com.team7.cmput301.android.theirisproject.IrisProjectApplication;
+import com.team7.cmput301.android.theirisproject.helper.StringHelper;
 import com.team7.cmput301.android.theirisproject.model.Contact;
 import com.team7.cmput301.android.theirisproject.model.Patient;
 
@@ -31,46 +32,51 @@ public class GetPatientListByContactTask extends AsyncTask<Contact, Void, List<P
 
     @Override
     protected List<Patient> doInBackground(Contact... contacts) {
-        List<Patient> result = new ArrayList<>();
+        List<String> emails = new ArrayList<>();
+        List<String> phones = new ArrayList<>();
 
+        for (Contact contact : contacts) {
+            emails.add(contact.getEmail());
+            phones.add(contact.getPhone());
+        }
         // For every contact, check if the contact's phone or email is a registered patient in the DB
         // Also make sure they are a Patient
-        for (Contact contact : contacts) {
-            // WHERE type = "PATIENT" AND (email = contactEmail OR email = contactPhone)
-            String query = "{\n" +
-                    "  \"query\": {\n" +
-                    "    \"bool\": {\n" +
-                    "      \"must\": [\n" +
-                    "    \t{ \"term\": { \"type\": \"PATIENT\" }},\n" +
-                    "    \t{ \"bool\": {\n" +
-                    "    \t\t\"should\": [\n" +
-                    "    \t\t\t{ \"term\": { \"email\": \"" + contact.getEmail() + "\" }},\n" +
-                    "    \t\t\t{ \"term\": { \"phoneNumber\": \"" + contact.getPhone() + "\" }}\n" +
-                    "    \t\t]\n" +
-                    "    \t}}\n" +
-                    "\t  ]\n" +
-                    "    }\n" +
-                    "  }\n" +
-                    "}";
 
-            Search get = new Search.Builder(query)
-                    .addIndex(IrisProjectApplication.INDEX)
-                    .addType("user")
-                    .build();
-            try {
-                SearchResult res = IrisProjectApplication.getDB().execute(get);
+        // WHERE type = "PATIENT" AND (email = contactEmail OR email = contactPhone)
+        String query = "{\n" +
+                "  \"query\": {\n" +
+                "    \"bool\": {\n" +
+                "      \"must\": [\n" +
+                "    \t{ \"term\": { \"type\": \"PATIENT\" }},\n" +
+                "    \t{ \"bool\": {\n" +
+                "    \t\t\"should\": [\n" +
+                "    \t\t\t{ \"terms\": { \"email\": " + StringHelper.generateJSONArrayString(emails) + " }},\n" +
+                "    \t\t\t{ \"terms\": { \"phoneNumber\": " + StringHelper.generateJSONArrayString(phones) + " }}\n" +
+                "    \t\t]\n" +
+                "    \t}}\n" +
+                "\t  ]\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
 
-                if (!res.isSucceeded()) {
-                    return null;
-                }
+        System.out.println("query is " + query);
 
-                result.addAll(res.getSourceAsObjectList(Patient.class, true));
-            } catch (IOException e) {
-                e.printStackTrace();
+        Search get = new Search.Builder(query)
+                .addIndex(IrisProjectApplication.INDEX)
+                .addType("user")
+                .build();
+        try {
+            SearchResult res = IrisProjectApplication.getDB().execute(get);
+
+            if (res.isSucceeded()) {
+                return res.getSourceAsObjectList(Patient.class, true);
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return result;
+        return null;
     }
 
     @Override
