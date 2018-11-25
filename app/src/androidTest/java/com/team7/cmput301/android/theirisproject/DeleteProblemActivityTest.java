e87c00dbb1f07324f24f7dfcb13283f9106bd01f
Justin Mah <jmmxpdev@gmail.com>
2018-11-24 18:34:16 -0700

    Fix javadoc typo

--- app/src/androidTest/java/com/team7/cmput301/android/theirisproject/DeleteProblemActivityTest.java
+++ app/src/androidTest/java/com/team7/cmput301/android/theirisproject/DeleteProblemActivityTest.java
@@ -14,7 +14,7 @@ import com.team7.cmput301.android.theirisproject.task.AddProblemTask;
 import com.team7.cmput301.android.theirisproject.task.Callback;
 
 /**
- * Tests for EditProfileActivity
+ * Tests for DeleteProblemActivity
  *
  * @author anticobalt
  */
