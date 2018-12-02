30f75953064522a13843ef2ce0f0cdb89152552d
Justin Mah <jmmxpdev@gmail.com>
2018-12-02 15:28:37 -0700

    Working geolocation queries

--- app/src/main/java/com/team7/cmput301/android/theirisproject/model/Record.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/model/Record.java
@@ -31,8 +31,14 @@ public class Record {
     private String title;
     private Date date = new Date();
     private GeoLocation geoLocation;
+    private double[] location = new double[2];
+
     private BodyLocation bodyLocation;
     transient private List<RecordPhoto> recordPhotos = new ArrayList<>();
+
+    public void setLocation(double[] location) {
+        this.location = location;
+    }
 
     /* Constructors */
 
