ed1d92747e7a28f503406ec7e4655413e01616d7
Justin Mah <jmmxpdev@gmail.com>
2018-12-02 16:55:30 -0700

    Working geolocation query

--- app/src/main/java/com/team7/cmput301/android/theirisproject/SearchState.java
+++ app/src/main/java/com/team7/cmput301/android/theirisproject/SearchState.java
@@ -5,10 +5,12 @@
 package com.team7.cmput301.android.theirisproject;
 
 
+import com.team7.cmput301.android.theirisproject.helper.StringHelper;
 import com.team7.cmput301.android.theirisproject.model.Problem;
 import com.team7.cmput301.android.theirisproject.model.Record;
 import com.team7.cmput301.android.theirisproject.task.Callback;
 import com.team7.cmput301.android.theirisproject.task.SearchBodyLocationTask;
+import com.team7.cmput301.android.theirisproject.task.SearchGeoLocationTask;
 import com.team7.cmput301.android.theirisproject.task.SearchKeywordTask;
 import com.team7.cmput301.android.theirisproject.task.SearchProblemsByRecordsTask;
 
@@ -65,6 +67,31 @@ public class SearchState {
     public static class SearchGeoLocation extends SearchState {
         public SearchGeoLocation() {}
 
+        @Override
+        public void querySearchProblems(String id, String keyword, List<Problem> problems, List<Record> records, Callback cb) {
+        }
+
+        @Override
+        public void querySearchRecords(String id, String keyword, List<Problem> problems, List<Record> records, Callback cb) {
+            String trim = keyword.trim();
+            String[] split = trim.split(",");
+            System.out.println(split[0]);
+            System.out.println(split[1]);
+
+            new SearchGeoLocationTask(new Callback<List<Record>>() {
+                @Override
+                public void onComplete(List<Record> res) {
+                    records.addAll(res);
+                    new SearchProblemsByRecordsTask(new Callback<List<Problem>>() {
+                        @Override
+                        public void onComplete(List<Problem> result) {
+                            problems.addAll(result);
+                            cb.onComplete(res);
+                        }
+                    }).execute(res);
+                }
+            }).execute(id, split[0], split[1]);
+        }
     }
 
     public static class SearchBodyLocation extends SearchState {
