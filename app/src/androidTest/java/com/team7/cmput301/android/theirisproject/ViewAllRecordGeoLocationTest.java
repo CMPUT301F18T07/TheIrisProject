7255e379257d42b4ea042220211f8b75caec3ad7
vinnyluu2 <vinnyluu2@gmail.com>
2018-12-02 16:43:43 -0700

    start on activity intent test

---
+++ app/src/androidTest/java/com/team7/cmput301/android/theirisproject/ViewAllRecordGeoLocationTest.java
@@ -0,0 +1,6 @@
+package com.team7.cmput301.android.theirisproject;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ViewAllRecordGeoLocationTest extends ActivityInstrumentationTestCase2 {
+}
